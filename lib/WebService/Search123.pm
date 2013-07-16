7d26c15dcdcc0af9ce0db814387be16813cf4151
Rob Brown <cagao@cpan.org>
2013-07-16 16:23:39 +0000

    Import of CAGAO/WebService-Search123-0.03 from CPAN.
    gitpan-cpan-distribution: WebService-Search123
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         CAGAO/WebService-Search123-0.03.tar.gz
    gitpan-cpan-author:       CAGAO
    gitpan-cpan-maturity:     released

--- lib/WebService/Search123.pm
+++ lib/WebService/Search123.pm
@@ -25,11 +25,11 @@ WebService::Search123 - Interface to the Search123 API.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 $VERSION = eval $VERSION;
 
